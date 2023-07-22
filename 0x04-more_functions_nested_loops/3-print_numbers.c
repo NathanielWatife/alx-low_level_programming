d4ebfedc20584d46c3041742426d81ac1452aeac
NathanielWatife <awosanminathaniel0@gmail.com>
2023-07-21 18:18:46 -0700

    commit print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - print 0-9
@@ -9,7 +10,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
-		_putchar(i + '0');
-	_putchar('\n');
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
 }
